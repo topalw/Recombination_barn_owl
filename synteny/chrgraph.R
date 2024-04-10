d4f88642ba9b61088d32da05eced085e5495d8d7
topalw <topalei@gmail.com>
2024-04-10 11:55:40 +0200

    modified synteny

--- synteny/chrgraph.R
+++ synteny/chrgraph.R
@@ -8,6 +8,8 @@ library(tidyverse)
 library(ggplotlyExtra)
 library(plotly)
 
+order <- read.csv('new_order.csv',h=F)
+names(order) <- c('lg','ss','chicken')
 l = args[1] 
   chrinfo <- read_delim("chrinfo.2.txt", delim = "\t")
   chrinfo <- group_by(chrinfo, speciescode) %>% arrange(krank) %>% mutate(offset = lag(seqlength), offset = replace_na(offset, 0), offset = cumsum(offset), xoffset = offset + ((krank - 1) * 2000000))
@@ -22,24 +24,37 @@ l = args[1]
                                cname = 'Tyto alba')
   dnazoo$seqlength[dnazoo$seqid=='Super-Scaffold_2'] <- 28222627
   renamer <- read.csv('../2.metadata/supp_table_lgs.csv')
-  renamer[24,2] <- '2.2'
+  renamer[1,2] <- '2.2'
   r2 <- cbind.data.frame('old'=gsub('^','s',renamer[,2]),
                    'new'=gsub('^','LG',renamer$Linkage.group))
   r2$old[r2$new == 'LG34'] <- 's2042'
   r2$old[r2$new == 'LG14'] <- 's106'
   dnazoo$chrname[match(r2$old,dnazoo$chrname)] <- r2$new
+  # reorder 
+  dnazoo$krank[match(order$lg,dnazoo$chrname)] <- c(seq(1,22),seq(25,nrow(order)+2))
+  # select useful lgs 
+  #good <- c(which(dnazoo$chrname %in% r2$new), which(dnazoo$chrname %in% c('s13','s42') & dnazoo$speciescode =='TYTAL'))
+  #dnazoo <- dnazoo[good,]
   
-  dnazoo <- group_by(dnazoo, speciescode) %>% arrange(krank) %>% mutate(offset = lag(seqlength), offset = replace_na(offset, 0), offset = cumsum(offset), xoffset = offset + ((krank - 1) * 2000000))
-  chrinfo <- bind_rows(dnazoo, select(chrinfo, colnames(dnazoo)))
+  # give useless scaffolds a higher krank (not present in linkage groups) to avoid plotting gaps 
+  useless <- which(! dnazoo$chrname %in% c(r2$old,r2$new,c('s42','s13')) & dnazoo$speciescode =='TYTAL')
+  dnazoo$krank[useless] <- seq(nrow(order)+3,nrow(order)+2 + length(useless)) 
 
+  
+  # make krank and offset based on chr
+  dnazoo <- group_by(dnazoo, speciescode) %>% 
+    arrange(krank) %>% 
+    mutate(offset = lag(seqlength), offset = replace_na(offset, 0), offset = cumsum(offset), xoffset = offset + ((krank - 1) * 2000000))
+  chrinfo <- bind_rows(dnazoo, select(chrinfo, colnames(dnazoo)))
+  # species info 
   speciesinfo.orig <- read_delim("species.txt", delim = "\t")
   speciesinfo <- tribble(~uniprotspeciescode,l,ref)
   speciesinfo <- left_join(speciesinfo, select(speciesinfo.orig, uniprotspeciescode, "Common name"), by = "uniprotspeciescode")
   speciesinfo <- speciesinfo %>% mutate(rphyloorder = row_number())
   chrinfo <- left_join(chrinfo, select(speciesinfo, uniprotspeciescode, rphyloorder), by = c("speciescode"="uniprotspeciescode"))
   chrinfo <- filter(chrinfo, ! is.na(rphyloorder))
-
-  minalnlen <- 100000 #100kb
+  # read alignment chainpairs 
+  minalnlen <- 100000 #100kb min length
   mergedaln <- list()
   for (i in 1:2){
     if (i == 1){
@@ -60,7 +75,6 @@ l = args[1]
     b <- NULL
     b <- read_delim(aln, "\t",col_names = F)
     colnames(b) <- c("tacc", "tstart", "tend", "qacc", "qstart", "qend", "strand")
-    if(lgs){
       if(i==2){
         # remove ss2 
         b <- b[b$qacc != 'Super-Scaffold_2',] 
@@ -74,7 +88,6 @@ l = args[1]
                          col_names = c("tacc", "tstart", "tend", "qacc", "qstart", "qend", "strand"))
         b <- rbind(tmp,b)
       }
-    }
     b <- left_join(b, dplyr::select(chrinfo, tacc = seqid, tchr = chrname, trank = krank, toffset=xoffset, torder = rphyloorder, tchrtype = chrtype, tcode = speciescode), by = "tacc") 
     b <- left_join(b, dplyr::select(chrinfo, qacc = seqid, qchr = chrname, qrank = krank, qoffset=xoffset, qorder = rphyloorder, qchrtype = chrtype, qcode = speciescode), by = "qacc")
     mergedaln[[i]] <- b 
@@ -88,9 +101,14 @@ l = args[1]
   c <- mutate(b, tlen=tend-tstart, qlen=qend-qstart) %>% 
     filter((qlen >= minalnlen | tlen >= minalnlen) & ! is.na(qchr) & ! is.na(tchr)) %>% 
     mutate(alnid = paste("aln", row_number(), sep = "")) %>%
-    mutate( alntype = paste(tchrtype, qchrtype, sep = ":"), x1 = tstart, x2 = tend, x1 = x1 + toffset, x2 = x2 + toffset, x3 = if_else(strand == '-', qstart + qoffset, qend + qoffset), x4 = if_else(strand == '-', qend + qoffset, qstart + qoffset), y1 = torder - 0.01, y2 = torder - 0.01, y3 = qorder + 0.16, y4 = qorder + 0.16) %>%
+    mutate(alntype = paste(tchrtype, qchrtype, sep = ":"), x1 = tstart, x2 = tend, x1 = x1 + toffset, x2 = x2 + toffset, x3 = if_else(strand == '-', qstart + qoffset, qend + qoffset), x4 = if_else(strand == '-', qend + qoffset, qstart + qoffset), y1 = torder - 0.01, y2 = torder - 0.01, y3 = qorder + 0.16, y4 = qorder + 0.16) %>%
     select(alnid,x1,x2,x3,x4,y1,y2,y3,y4,alntype)
-
+  # flip assembly 
+  tmpor <- which(c$x3 < c$x4)
+  tmp <- c$x3[tmpor] 
+  c$x3[tmpor] <- c$x4[tmpor]
+  c$x4[tmpor] <- tmp
+  
   t1 <- select(c, alnid, alntype, x1:x4) %>% pivot_longer(x1:x4, names_to = "ctype", values_to = "xcoords") %>% select(alnid, alntype, xcoords)
   t2 <- select(c, alnid, alntype, y1:y4) %>% pivot_longer(y1:y4, names_to = "ctype", values_to = "ycoords") %>% select(alnid, alntype, ycoords)
   t <- bind_cols(t1, t2)
@@ -103,11 +121,17 @@ l = args[1]
   tytal <- tytal[grep('LG',tytal$chrname),]
   chrinfo <- rbind(tmp,tytal,z)
   chrinfo <- bind_rows(chrkept,chrinfo)
+  # colour Z 
+  chrinfo$chrtype[chrinfo$chrname == 'chrZ'] <- 'Z'
+  chrinfo$chrtype[chrinfo$chrname %in% c('s13','s42')] <- 'Z'
+  chrinfo <- chrinfo[chrinfo$rphyloorder!=3,] # remove top row 
+  t <- t[t$ycoords < 2,] # keep only barn owl to chicken 
   # plot 
-  p <- ggplot() + geom_rect(data = chrinfo, aes(xmin=xoffset, xmax=xoffset+seqlength, ymin=rphyloorder,ymax= rphyloorder+0.15, fill=chrtype), alpha = 0.6) +
-    geom_text(data = chrinfo, aes(x=xoffset, y = jitter(rphyloorder + 0.075,amount=0.03), label = gsub('chr','',gsub('LG','',chrinfo$chrname))), hjust = 0, size = 2) + 
+  p <- ggplot() + 
+    geom_rect(data = chrinfo, aes(xmin=xoffset, xmax=xoffset + seqlength, ymin=rphyloorder,ymax= rphyloorder+0.15, fill=chrtype), alpha = 0.4) +
+    geom_text(data = chrinfo, aes(x=xoffset, y = jitter(rphyloorder + 0.075,amount=0.01), label = gsub('chr','',gsub('LG','',chrinfo$chrname))), hjust = 0, size = 2.5) + 
     geom_polygon(data = t, aes(x=xcoords,y=ycoords, fill = alntype, group = alnid), alpha = 0.4) +
-    scale_y_continuous(breaks = 1:nrow(speciesinfo)+ 0.075, labels = arrange(speciesinfo, rphyloorder+0.075) %>% pull("Common name")) +
+    scale_y_continuous(breaks = 1:nrow(speciesinfo[c(1,2),])+ 0.075, labels = arrange(speciesinfo[c(1,2),], rphyloorder[c(1,2)]+0.075) %>% pull("Common name")) +
     theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(), 
 	  text = element_text(size = 16), 
 	  axis.title = element_blank(),
@@ -116,11 +140,14 @@ l = args[1]
 
  print(p)
   
-pdf(paste0(ref,'vs',l,'.pdf'),width=16,height=10)
+pdf(paste0(ref,'vs',l,'.pdf'),width=12,height=10)
 print(p) 	
-#ggplotly(p)
- dev.off()
+dev.off()
+ 
+jpeg(paste0(ref,'vs',l,'.jpeg'),width=12,height=10,unit='in',quality = 1000,res=800, antialias = "cleartype" )
+print(p)
+dev.off()
 
-#  htmlwidgets::saveWidget(partial_bundle(ggplotly(p)), paste0(ref,"vs",l,".html"))
-
-
+ 
+ 
+ #  htmlwidgets::saveWidget(partial_bundle(ggplotly(p)), paste0(ref,"vs",l,".html"))
